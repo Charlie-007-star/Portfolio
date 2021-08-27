a6866b158b9a29a2132cb5d5a2cc4a90ed38ce8b
Charlie-007-star <sujinsr2457@gmail.com>
2021-08-27 06:31:37 +0530

    form done

--- mail/contact.js
+++ mail/contact.js
@@ -1,65 +1,176 @@
-$(function () {
 
-    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function ($form, event, errors) {
-        },
-        submitSuccess: function ($form, event) {
-            event.preventDefault();
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var subject = $("input#subject").val();
-            var message = $("textarea#message").val();
 
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
+// for form validation
+var formvalid = {
 
-            $.ajax({
-                url: "contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
-            });
-        },
-        filter: function () {
-            return $(this).is(":visible");
-        },
-    });
+    username: false,
+    mobilename: false,
+    messagename: false,
+    emailname: false
+  
+};
 
-    $("a[data-toggle=\"tab\"]").click(function (e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
+//Functions for checking validation 
+
+function validname(){
+     
+    function hide(){
+        $('#name-error').hide();
+    }
+    var username= $("#name").val();
+    console.log(username);
+    if(username.length<1){
+        formvalid.username = false;
+        $('#name-error').text('Character is required.').show();
+       
+    } else {
+       hide();
+       formvalid.username = true;
+      
+        var testExp= new RegExp(/^[a-zA-Z ]+$/);
+        var testEx= new RegExp(/^[a-zA-Z ]+$/);
+        if (!testExp.test(username)){
+            $('#name-error').text('Must not have any special character or number.').show();
+            formvalid.username = false;
+           
+        } else {
+           hide();
+           formvalid.username = true;
+         
+            if (username.length<3 || username.length > 20){
+                $('#name-error').text('Must be more than 3 character and lesser than 20 character').show();
+                formvalid.username = false;
+              
+            }
+            else if(username.match(testEx)>3){
+                $('#name-error').text('Must be more than 3 character and lesser than 20 character').show();
+                formvalid.username = false;
+              
+                console.log(username.match(testEx))
+            }
+             else {
+                hide();
+                formvalid.username = true;
+               
+            }
+        }
+        
+        
+    }
+}
+
+
+
+// mobile
+
+function validmobile(){
+    var mobilename = $("#mobile").val();
+
+    function mobilehide(){
+        $('#mobile-error').hide();
+    }
+
+    if(mobilename.length<1){
+        $('#mobile-error').text('Only 10 numbers required.').show();
+        formvalid.mobilename = false;
+       
+    } else {
+        mobilehide();
+        formvalid.mobilename = true;
+       
+    if(mobilename.length<10){
+        $('#mobile-error').text('Must have 10 numbers.').show();
+        formvalid.mobilename = false;
+       
+    } else {
+        mobilehide();
+        formvalid.mobilename = true;
+       
+    }
+    }
+}
+
+// message
+function validmessage(){
+    var messagename =$("#message").val();
+
+    function messagehide(){
+        $('#message-error').hide();
+    }
+
+    if(messagename.length<1){
+        $('#message-error').text('20 characters minimum required.').show();
+        formvalid.messagename = false;
+       
+    } else {
+        messagehide();
+        formvalid.messagename = true;
+       
+        if(messagename.length<20){
+            $('#message-error').text('Atleast 20 Characters required.').show();
+            formvalid.messagename = false;
+           
+        } else {
+            messagehide();
+            formvalid.messagename = true;
+         
+        }
+    }
+}
+
+// email
+
+function validemail(){
+    var emailname = $("#email").val();
+
+    function emailhide(){
+        $('#email-error').hide();
+    }
+
+
+    if(emailname.length<1){
+        $('#email-error').text('Email is required').show();
+        formvalid.emailname = false;
+       
+    } else {
+        emailhide();
+        formvalid.emailname =  true;
+       
+        var em = new RegExp(/^[^]+@[^]+\.[a-z]{2,3}$/);
+        if(!em.test(emailname)){
+            $('#email-error').text('Enter only valid email only.').show();
+            formvalid.emailname = false;
+           
+        } else {
+            emailhide();
+            formvalid.emailname = true;
+            
+        }
+    }
+
+}
+// name 
+$('#name').on('input',function(){
+
+   validname();
 });
 
-$('#name').focus(function () {
-    $('#success').html('');
+
+// mobile number
+$('#mobile').on('input',function(){
+   
+    validmobile();
 });
+
+// message box
+$('#message').on('input',function(){
+  
+    validmessage();
+});
+
+// email 
+$('#email').on('input',function(){
+   
+    validemail();
+
+})
